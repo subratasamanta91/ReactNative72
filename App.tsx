415946eec7cf9b104ccac02fe2256210e388fbdf
subratasamanta91 <subratasamanta91@gmail.com>
2023-12-23 16:12:20 +0530

    app file changes

--- App.tsx
+++ App.tsx
@@ -1,22 +1,10 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
   ScrollView,
-  StatusBar,
-  StyleSheet,
   Text,
-  useColorScheme,
   View,
 } from 'react-native';
-
 function App(): JSX.Element {
   return (
     <SafeAreaView>
