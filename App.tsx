66b15cd5e1e7ee880249faedcb83dd2c7afe8426
subratasamanta91 <subratasamanta91@gmail.com>
2023-12-23 14:11:50 +0530

    App fixed

--- App.tsx
+++ App.tsx
@@ -17,20 +17,15 @@ import {
   View,
 } from 'react-native';
 
-
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
-
   return (
     <SafeAreaView>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'}/>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
         <View>
           <Text>React Native 72.8</Text>
@@ -39,24 +34,4 @@ function App(): JSX.Element {
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
-
 export default App;
